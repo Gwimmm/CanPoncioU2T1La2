90fe38c2866b1ab09861952d33d4e81152fdea70
Ronaldo Can Poncio <ronaldoc25@nycstudents.net>
2022-09-30 09:28:29 -0400

    changed the coding for Introduction. (Cat) added jam and jar cat.

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,5 +1,11 @@
 public class CatRunner {
     public static void main(String[] args) {
-        //testing commit ush
+        Cat cat1 = new Cat("Jar", 8, 95);
+        cat1.introduce();
+        cat1.printCatInfo();
+
+        Cat cat2 = new Cat("Jam", 4, 78);
+        cat2.introduce();
+        cat2.printCatInfo();
     }
 }
